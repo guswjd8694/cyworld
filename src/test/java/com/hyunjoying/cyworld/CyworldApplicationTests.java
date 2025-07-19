7db249e44ec91a47f89e965bfb96d0198037fe70
guswjd8694 <ajdrns8694@gmail.com>
2025-07-19 15:51:00 +0900

    :sparkles: Initial Commit

---
+++ src/test/java/com/hyunjoying/cyworld/CyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunjoying.cyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
